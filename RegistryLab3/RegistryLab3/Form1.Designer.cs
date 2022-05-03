7137febaa8140c048dfb2821d5b980c9bebd651a
Ramunis <seva.m@live.com>
2022-05-03 13:54:11 +1000

    Regedit v1.0

---
+++ RegistryLab3/RegistryLab3/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace RegistryLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(357, 242);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(349, 216);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "5.4";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вернуть назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Убрать слово";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Задание: \r\nУбираем слова \"Ярлык для...\" при создании ярлыков";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(349, 216);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "5.5";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Вернуть назад";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Убрать стрелки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Задание: \r\nУбираем стрелки с ярлыков";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button6);
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(349, 216);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "5.6";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 185);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Вернуть назад";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 60);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Отключить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Задание: \r\nОтключение диспетчера задач Windows";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button9);
+            this.tabPage4.Controls.Add(this.button8);
+            this.tabPage4.Controls.Add(this.button7);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(349, 216);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "5.7";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(11, 185);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(114, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Вернуть назад";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.ForeColor = System.Drawing.Color.Red;
+            this.button7.Location = new System.Drawing.Point(11, 89);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(141, 48);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "НЕ НАЖИМАТЬ НА ЭТУ КНОПКУ!!!";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 26);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Задание: \r\nОграничение запуска программ";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(11, 60);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(141, 23);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "Снять предохранитель";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 242);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Ramunis B4 214.31 OC Registry Lab3";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ RegistryLab3/RegistryLab3/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RegistryLab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer", true);
+            wKey.SetValue("link", "0");
+            wKey.Close();
+            MessageBox.Show("Сделано");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer", true);
+            wKey.SetValue("link", "1e 00 00 00");
+            wKey.Close();
+            MessageBox.Show("Возвращего как было");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.LocalMachine;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Classes\\lnkfile", true);
+            wKey.DeleteValue("IsShortcut");
+            wKey.Close();
+            MessageBox.Show("Удалено");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.LocalMachine;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Classes\\lnkfile", true);
+            wKey.SetValue("IsShortcut","");
+            wKey.Close();
+            MessageBox.Show("Возвращено как было");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies", true); //ВЕТКА
+            wKey.CreateSubKey("System"); //СОЗДАЕМ
+            RegistryKey restKey = wKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true); //ПОДВЕТКА
+            restKey.SetValue("DisableTaskMgr",1); //ПРОПИСЫВЕМ
+            restKey.Close(); //ЗАКРЫВАЕМ
+            wKey.Close(); //ЗАКРЫВАЕМ
+            MessageBox.Show("Отключено");
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies", true);
+            RegistryKey restKey = wKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+            restKey.SetValue("DisableTaskMgr", 0);
+            restKey.DeleteValue("DisableTaskMgr");
+            restKey.Close();
+            wKey.DeleteSubKey("System");
+            wKey.Close();
+            MessageBox.Show("Возвращено как было");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", true); //ВЕТКА
+            wKey.SetValue("RestrictRun", 1); //ПРОПИСЫВАЕМ
+            wKey.CreateSubKey("RestrictRun"); //СОЗДАЕМ
+            RegistryKey restKey = wKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\RestrictRun", true); //ПОДВЕТКА
+            restKey.SetValue("NoDriveType", 145); //ПРОПИСЫВАЕМ 
+            restKey.SetValue("RestrictRun", 1);
+            restKey.SetValue("C:\\Windows\\regedit.exe", Application.ExecutablePath); // ПРОПИСЫВЕМ ПРИЛОЖЕНИЯ
+            restKey.SetValue("C:\\Windows\\explorer.exe", Application.ExecutablePath);
+            restKey.SetValue("C:\\Windows\\notepad.exe", Application.ExecutablePath);
+            restKey.SetValue("C:\\Program Files(x86)\\Microsoft\\Edge\\Application\\msedge.exe", Application.ExecutablePath);
+            restKey.SetValue("C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE", Application.ExecutablePath);
+            restKey.SetValue("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE", Application.ExecutablePath);
+            restKey.SetValue("C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.EXE", Application.ExecutablePath);
+            restKey.Close(); //ЗАКРЫВАЕМ
+            wKey.Close(); //ЗАКРЫВАЕМ
+            MessageBox.Show("Отключено");
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            RegistryKey myKey = Registry.CurrentUser;
+            RegistryKey wKey = myKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", true);
+            wKey.SetValue("RestrictRun", 0);
+            wKey.DeleteSubKey("RestrictRun");
+            wKey.DeleteValue("RestrictRun");
+            wKey.Close();
+            MessageBox.Show("Возвращено как было");
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button7.Enabled = true;
+        }
+    }
+}
